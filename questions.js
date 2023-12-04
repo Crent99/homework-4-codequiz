927f105e615275433ef4eb768890e2daf082a676
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2023-12-04 03:11:27 -0500

    added CSS

--- questions.js
+++ questions.js
@@ -1,3 +1,4 @@
+// var with coding questions
 var questions = [
     {
         title: "Which function is used to serialize an object into a JSON string in Javascript?",
@@ -8,7 +9,7 @@ var questions = [
         title: "Which of the following are closures in Javascript?",
         choices: ["Variables", "Functions", "Objects", "All of the above"],
         answer: "All of the above"
-    }, 
+    },
     {
         
         title: "How can a datatype be declared to be a constant type?",
@@ -27,7 +28,8 @@ var questions = [
         choices: ["Object-Oriented", "Object-Based", "Procedual", "None of the Above"],
         answer: "Object-Oriented"
 
-    }
+    },
 
 
-]
+];
+
