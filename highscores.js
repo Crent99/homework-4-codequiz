c7f9d5b2abe81ca9554167ab26e340e8d7c5c797
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2023-12-28 11:45:50 -0500

    restarted part2

--- highscores.js
+++
@@ -1,8 +0,0 @@
-var goBack = document.querySelector("#goBack");
-var clearScores = document.querySelector("#clearScores");
-var highScores = document.querySelector("#highScores");
-var scoreList = document.querySelector("#scoreList");
-var scores = [];
-
-
-
